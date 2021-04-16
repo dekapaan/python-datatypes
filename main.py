865087123f46497fd20c3070066e8794d8c01d9a
dekapaan <terblancheronald@gmail.com>
2021-04-16 08:41:20 +0200

    Datatypes exercises

---
+++ main.py
@@ -0,0 +1,10 @@
+numbers = [5, 1, 3, 2, 4]
+y = 0
+for x in numbers:
+    if x > y:
+        y = x
+print(y)
+print("")
+numbers1 = [10, 2, 45, 3, 5, 7, 2, 10, 45, 8, 10]
+numbers1 = list(set(numbers1))
+print(numbers1)
